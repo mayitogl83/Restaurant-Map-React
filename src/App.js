e969f7779e6eb00d35bc48c21a47b40d972fcba3
Mario Gonzalez <mayitogl83@gmail.com>
2018-09-28 22:26:09 -0500

    Add API google map callback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.loadMap()
+  }
+
+  loadMap = ()=> {
+    addScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyC3QzWGUHpJnCpzT2cItd6A5cahBuBDyI0&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap =()=> {
+    var map = new window.google.maps.Map
+    (document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 13
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="map"></div>
       </div>
     );
   }
+}
+
+function addScript(url) {
+    var index = window.document.getElementsByTagName('script')[0]
+    var script = window.document.createElement('script')
+    script.src = url
+    script.async = true
+    script.defer = true
+    index.parentNode.insertBefore(script, index)
 }
 
 export default App;
