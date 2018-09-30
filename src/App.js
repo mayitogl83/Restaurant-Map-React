90b53fb1f4962de847e5426b882731dffff17541
Mario Gonzalez <mayitogl83@gmail.com>
2018-09-30 00:06:00 -0500

    Add initial marker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
+    this.addVenues()
     this.loadMap()
   }
 
@@ -15,9 +21,37 @@ class App extends Component {
   initMap =()=> {
     var map = new window.google.maps.Map
     (document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
+      center: {lat: 40.7413549, lng: -73.9980244},
       zoom: 13
     });
+
+   var newyork = {lat: 40.7413549, lng: -73.9980244};
+    var marker = new window.google.maps.Marker({
+            position: newyork,
+            map: map,
+            title: 'My Marker!'
+    });
+  }
+
+  addVenues = ()=> {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "IUEOXWI5VCY3RMI2ISMBM4AEH54TJM3YBDEZ5KMYLRNLTDR5",
+      client_secret: "D0XCWFQ5PZHSK43AJHYZN41ZU30O52BCLBWW5XHUN5MQYEQU",
+      query: "food",
+      near: "New York",
+      v: "20180323"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({
+        venues: response.data.response.groups[0].items
+      })
+    })
+    .catch(error => {
+      console.log("ERROR" + error)
+    })
   }
 
   render() {
