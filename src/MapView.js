db5f6d619c01908cac32b7cb9a34239aae1b6b79
Mario Gonzalez <mayitogl83@gmail.com>
2018-10-12 22:56:44 -0500

    Add map view in separate file

--- src/MapView.js
+++ src/MapView.js
@@ -2,31 +2,11 @@ import React, {Component} from 'react';
 
 class MapView extends Component {
 
-  openNav = ()=> {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-  closeNav = ()=> {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}
-
   render() {
     return(
-        <div className="container">
-          <div id="mySidenav" className="sidenav">
-            <a className="closebtn" onClick={this.closeNav}>&times;</a>
-            <input id="search" className="search" placeholder="search"></input>
-          </div>
-          <div id="main"className="main">
-          <div className="top-bar">
-            <span className="menu" onClick={this.openNav}>&#9776;</span>
-            <h4 className="header">Neigborhood Map</h4>
-          </div>
-          <div id="map"></div>
-          </div>
-        </div>
+      <div className="map-container">
+        <section id="map" className="map" role="application" tabIndex="-1"></section>
+      </div>
     )
   }
 }
